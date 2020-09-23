a66dcc1addb46fdb9e7a09d464345715872cf9b2
Rene M Paccha <1162669+renemarcelo@users.noreply.github.com>
2020-09-23 18:57:19 -0500

    drawing up an example use-se for webflow-only users

--- test/webflow.test.js
+++ test/webflow.test.js
@@ -2,5 +2,6 @@ const expect = require('chai').expect
 const webflow = require('../lib/webflow')
 
 describe('Webflow', () => {
-  
-})
+  xit('should not have you require extra non-webflow setup for ``--to webflow` crosspost option', () => {
+
+  })})
